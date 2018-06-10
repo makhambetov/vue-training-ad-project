d03b59430f1d1bf666cb618baeb3d62120699347
Ivan Makhambetov <i.makhambetov@mitwork.net>
2018-06-10 20:33:31 +0600

    menu added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <template>
   <v-app>
-    <v-navigation-drawer app temporary></v-navigation-drawer>
-    <v-toolbar app>
 
-      <template>
-        <v-toolbar app dark color="primary">
-          <v-toolbar-side-icon></v-toolbar-side-icon>
-          <v-toolbar-title>Title</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-toolbar-items class="hidden-sm-and-down">
-            <v-btn flat>Link One</v-btn>
-          </v-toolbar-items>
-        </v-toolbar>
-      </template>
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-list>
+        <v-list-tile
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url"
+        >
+          <v-list-tile-action>
+            <v-icon color="pink">{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app>
+      <v-toolbar app dark color="primary">
+        <v-toolbar-side-icon
+          @click="drawer = !drawer"
+          class="hidden-md-and-up"
+        >
+        </v-toolbar-side-icon>
+        <v-toolbar-title>Title</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn
+            flat
+            v-for="link in links"
+            :to="link.url"
+          >
+            <v-icon left>{{link.icon}}</v-icon>
+            {{link.title}}
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
 
     </v-toolbar>
     <v-content>
@@ -24,7 +49,18 @@
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data () {
+      return {
+        drawer: false,
+        links: [
+          {title: 'Login', icon: 'lock', url: '/login'},
+          {title: 'Registration', icon: 'face', url: '/registration'},
+          {title: 'Orders', icon: 'bookmark_border', url: '/orders'},
+          {title: 'New Ad', icon: 'note_add', url: '/new'},
+          {title: 'My ads', icon: 'list', url: '/list'}
+        ]
+      }
+    }
+  }
 </script>
